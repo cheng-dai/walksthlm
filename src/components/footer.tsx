07d9eb334e4f2d5defa8a32b4d6d9509c91ddc49
chengdai <chengdai.dev@gmail.com>
2024-03-11 12:15:12 +0100

    changed footer to fixed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="sticky -z-10 bottom-2 tracking-wider max-md:text-sm">
+    <div className="fixed -z-10 bottom-2 tracking-wider max-md:text-sm">
       built with ❤️ by <a href='https://www.chengdai.dev' className=' font-mono bg-gradient-to-br from-[#3a91c6] to-[#750c45] text-transparent bg-clip-text' target='_blank'>cheng</a>
     </div>
   )
