c2b6e183d34e2eedc1463a9192bfd79e21d63b86
chengdai <chengdai.dev@gmail.com>
2024-03-13 12:19:56 +0100

    added font

--- src/components/event.tsx
+++ src/components/event.tsx
@@ -9,7 +9,7 @@ export default function Event() {
           <span className='font-semibold'>日期</span>: 2024.3.23 周六</p>
           <p><span className='font-semibold'>时间</span>: 上午10:30</p>
           <p><span className='font-semibold'>地点</span>: KTH Entré</p>
-          <p><span className='font-semibold'>详情</span>: 👉 <a href='https://www.xiaohongshu.com/discovery/item/65eb8197000000000b00d8bf?app_platform=ios&app_version=8.25.4&author_share=1&share_from_user_hidden=true&type=normal&xhsshare=WeixinSession&appuid=6053b36f000000000101e54f&apptime=1709964693' className='underline underline-offset-4' target='_blank'>小红书笔记</a></p>
+          <p><span className='font-semibold'>详情</span>: <a href='https://www.xiaohongshu.com/discovery/item/65eb8197000000000b00d8bf?app_platform=ios&app_version=8.25.4&author_share=1&share_from_user_hidden=true&type=normal&xhsshare=WeixinSession&appuid=6053b36f000000000101e54f&apptime=1709964693' className='underline underline-offset-4' target='_blank'>小红书笔记</a></p>
           </div>
         <Map />
         
